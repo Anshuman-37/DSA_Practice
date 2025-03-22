5e7cd9b4049875666eeb885a42fc21a566f501aa
Anshuman Singh <asingh99cool@gmail.com>
2025-03-22 14:33:42 +0530

    Sliding Window: Longest Substring with K Distinct Characters Leetcode

---
+++ Arrays/longest_substring_with_at_most_k_distinct_char.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        max_size = 0
+        counter = Counter()
+
+        for right in range(len(s)):
+            counter[s[right]] += 1
+
+            if len(counter) <= k:
+                max_size += 1
+            else:
+                counter[s[right - max_size]] -= 1
+                if counter[s[right - max_size]] == 0:
+                    del counter[s[right - max_size]]
+
+        return max_size
