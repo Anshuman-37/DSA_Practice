2bd6dd3d2824928929b41c9691bc181cc258098c
Anshuman Singh <asingh99cool@gmail.com>
2025-03-29 17:10:28 +0530

    Dynamic Programming: Count Ways To Build Good Strings Leetcode

--- Dynamic Programming/count_ways_to_build_good_strings.py
+++ Dynamic Programming/count_ways_to_build_good_strings.py
@@ -12,3 +12,37 @@ class Solution:
             dp[end] %= mod
         # ADd the number of strings with each valid length [low ~ high]
         return sum(dp[low:high+1]) % mod
+
+def something():
+    str1 = str1.lower().split("")
+    str2 = str2.lower().split("")
+
+    len_str1 = len(str1)
+    len_str2 = len(str2)
+
+    # Base Case
+    if not str1 and not str2:
+        return False
+
+    i, j = 0, 0
+
+    if len_str1 > len_str2:
+        str1, str2 = str2, str1
+
+    while str1[i] and str2[j]:
+        if str1[i] == str2[j]:
+            i += 1
+            j += 1
+        if str1[i] != str2[j]:
+            j += 1
+
+    if i == j:
+        return True
+
+    if i != len_str1:
+        return False
+
+    return abs(len_str1 - len_str2) > 2
+
+str1 = " She is amazing"
+print(str1.lower().split(" "))
