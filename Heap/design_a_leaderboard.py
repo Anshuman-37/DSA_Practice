9ab33a05feeebbaabfaf9428a8e93004200e616d
Anshuman Singh <asingh99cool@gmail.com>
2025-04-07 15:05:48 +0530

    Heap: Design a Leaderboard Leetcode

---
+++ Heap/design_a_leaderboard.py
@@ -0,0 +1,16 @@
+from heapq import nlargest
+
+
+class Leaderboard:
+    def __init__(self):
+        self.scores = {}
+
+    def addScore(self, playerId: int, score: int) -> None:
+        self.scores[playerId] = self.scores.get(playerId, 0) + score
+
+    def top(self, K: int) -> int:
+        return sum(nlargest(K, self.scores.values()))
+
+    def reset(self, playerId: int) -> None:
+        if playerId in self.scores:
+            del self.scores[playerId]
